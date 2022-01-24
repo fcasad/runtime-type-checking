d3ea91ebad225f308beaf4056151a5f90e07f7b8
Farren <fcasad@gmail.com>
2022-01-24 01:18:24 -0800

    json schema generated from ts

--- src/validation.ts
+++ src/validation.ts
@@ -1,71 +1,7 @@
 import Ajv, { ValidateFunction } from 'ajv';
 
 import { SpaceObject } from './types';
-
-const schema = {
-  definitions: {
-    Vector: {
-      type: 'array',
-      items: { type: 'number' },
-      minItems: 3,
-      maxItems: 3,
-    },
-    BaseSpaceObject: {
-      type: 'object',
-      properties: {
-        location: { $ref: '#/definitions/Vector' },
-        mass: { type: 'number' },
-        name: { type: 'string' },
-      },
-      required: ['location', 'mass', 'name'],
-    },
-    Asteroid: {
-      type: 'object',
-      allOf: [{ $ref: '#/definitions/BaseSpaceObject' }],
-      properties: {
-        type: { const: 'asteroid' },
-      },
-      required: ['type'],
-    },
-    Planet: {
-      type: 'object',
-      allOf: [{ $ref: '#/definitions/BaseSpaceObject' }],
-      properties: {
-        type: { const: 'planet' },
-        population: { type: 'number' },
-        habitable: { type: 'boolean' },
-      },
-      required: ['type', 'population', 'habitable'],
-    },
-    Rank: {
-      type: 'string',
-      enum: ['captain', 'first mate', 'officer', 'ensign'],
-    },
-    CrewMember: {
-      type: 'object',
-      properties: {
-        name: { type: 'string' },
-        age: { type: 'number' },
-        rank: { $ref: '#/definitions/Rank' },
-        home: { $ref: '#/definitions/Planet' },
-      },
-      required: ['name', 'age', 'rank', 'home'],
-    },
-    Ship: {
-      type: 'object',
-      allOf: [{ $ref: '#/definitions/BaseSpaceObject' }],
-      properties: {
-        type: { const: 'ship' },
-        crew: { type: 'array', items: { $ref: '#/definitions/CrewMember' } },
-      },
-      required: ['type', 'crew'],
-    },
-    SpaceObject: {
-      type: 'object',
-      anyOf: [{ $ref: '#/definitions/Asteroid' }, { $ref: '#/definitions/Planet' }, { $ref: '#/definitions/Ship' }],
-    },
-  },
-};
+import schema from './schema.json';
 
 const ajv = new Ajv({ schemas: [schema] });
 
