28340135b1a092f973b8be38fea6e8ddfd36df22
Farren <fcasad@gmail.com>
2022-01-29 22:38:35 -0800

    bonus: use generated json-schema with swagger

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,37 @@
-import express from 'express';
+import cloneDeepWith from 'lodash/cloneDeepWith';
+import express, { Request, Response, NextFunction } from 'express';
+import swaggerUI, { JsonObject } from 'swagger-ui-express';
 
+import swaggerDoc from './swagger.json';
+import schema from './schema.json';
 import { isSpaceObject } from './validation';
 
 const app = express();
 
 app.use(express.json());
+
+type SwaggerDocsRequest = Request & {
+  swaggerDoc?: JsonObject;
+};
+
+function makeSwaggerSchemas(): object {
+  return cloneDeepWith(schema.definitions, (value, key) => {
+    if (key === '$ref' && typeof value === 'string') {
+      return value.replace('#/definitions', '#/components/schemas');
+    }
+  });
+}
+
+app.use(
+  '/v1/api-docs',
+  (req: SwaggerDocsRequest, res: Response, next: NextFunction) => {
+    swaggerDoc.components.schemas = makeSwaggerSchemas();
+    req.swaggerDoc = swaggerDoc;
+    next();
+  },
+  swaggerUI.serve,
+  swaggerUI.setup(),
+);
 
 app.post('/v1/space-objects', (req, res) => {
   if (!isSpaceObject(req.body)) {
